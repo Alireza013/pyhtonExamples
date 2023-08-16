1cab786fbeccf73e2257686312a72adb3e754162
Alireza Rahimi <85294640+Alireza013@users.noreply.github.com>
2023-08-16 10:44:53 +0330

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,11 +1,10 @@
+MAXIMUM_TRIERS = 1000
 
-def multiples():
-    i = 1
-    sum = 0
-    while i < 1000:
-        if (i % 3 == 0) or (i % 5 == 0) :
-            sum = sum + i
-        i += 1
+def multiples(item = 10, sum = 23) -> int:
+    while item < MAXIMUM_TRIERS:
+        if (item % 3 == 0) or (item % 5 == 0):
+            sum += item
+        item += 1
     print(sum)
 
 multiples()
